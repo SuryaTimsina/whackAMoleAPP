93d71a1652920a87ffe9d9725ef0ab5796ce706d
SuryaTimsina <113369865+SuryaTimsina@users.noreply.github.com>
2024-02-13 18:23:02 -0700

    Add files via upload

---
+++ styles/page-styles.js
@@ -0,0 +1,45 @@
+/*stylesheet*/
+
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    grid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 120,
+        height: 120,
+        margin: 2,
+    },
+    scoreCount: {
+        fontSize: 40,
+        color: 'blue',
+    },
+    livesCount: {
+        fontSize: 30,
+        color: 'red',
+    },
+    button: {
+        backgroundColor: 'black',
+        borderRadius: 30,
+        padding: 10,
+        marginVertical: 10,
+        height:50,
+        
+        
+    },
+    background: {
+        width: '100%',
+        minWidth: '100%',
+        height: '100%',
+        objectFit: 'cover',
+    },
+})
+export default styles;
